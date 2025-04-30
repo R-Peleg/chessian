1d3ea5dd99ae3fa6061964151a6575a707c34016
Reuven Peleg <4018286+R-Peleg@users.noreply.github.com>
2025-04-30 21:30:43 +0300

    Add new engine configuration for classic feature alpha beta with Stockfish

--- match_command.bat
+++ match_command.bat
@@ -3,3 +3,6 @@
 
  # 3^2 alpha beta with gemini vs. classic eval + moves
  C:\Users\ruby\chess\cutechess-1.3.1-win64\cutechess-cli.exe -engine name="classic alpha beta" cmd=C:\Users\ruby\projects\chessian\alpha_beta_engine.bat proto=uci st=120 -engine name="Gemini 2.0 Flash ab(3^2)" cmd=C:\Users\ruby\projects\chessian\alpha_beta_gem_engine.bat proto=uci st=120 stderr=gemini_err.log -rounds 2 -pgnout match_gem_20_flash_ab_vs_classic_ab.pgn -debug
+
+ # 1000 nodes for each engine
+ C:\Users\ruby\chess\cutechess-1.3.1-win64\cutechess-cli.exe -engine name="classic feature alpha beta" cmd=C:\Users\ruby\projects\chessian\ab_classic_features.bat proto=uci tc=inf nodes=1000 -engine name="Stockfish (1350)" cmd="C:\Users\ruby\chess\stockfish-windows-x86-64-avx2.exe" proto=uci option.UCI_LimitStrength=true option.UCI_Elo="1350" tc=inf nodes=1000 -rounds 10 -pgnout match_sf_classic_features4.pgn -debug
